afdd694296339efad27bb7d04f5dac3538f98fba
Mark Tyler <mark.tyler@tdsinc.com>
2015-03-12 11:29:46 -0500

    Got notes from controller, added to page.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var notelyBasePath = 'https://elevennote-nov-2014.herokuapp.com/api/v1/';
+var apiKey = '$2a$10$0nJlKJMRwXhYvftMeVbPu.lTUUJ6qd8xhbT2s4AhGThPLde01xcnK';
+
+
 // Declare app level module which depends on views, and components
 angular.module('myApp.notes', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
@@ -8,6 +12,9 @@ angular.module('myApp.notes', ['ngRoute'])
     controller: 'NotesController'
   });
 }])
-.controller('NoteController', function() {
-
-});
+.controller('NotesController', ['$http', '$scope', function($http, $scope) {
+  $http.get(notelyBasePath + 'notes.json?api_key=' + apiKey)
+  .success(function(notes_data) {
+      $scope.notes = notes_data;
+  });
+}]);
